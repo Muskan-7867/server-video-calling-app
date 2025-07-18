3fe158eb6c1edb62e8c3ee1c264420e43877ccb1
Muskan-7867 <muskanloach984@gmail.com>
2025-07-18 17:52:32 +0530

    delete

--- config/app.config.js
+++
@@ -1,9 +0,0 @@
-const MONGO_DB_CONFIG = {
- DB : "mongodb+srv://leatherwebmaker123:acJAxwz6CpBlo1hq@cluster0.4l8pcbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-}
-
-module.exports = {
-    MONGO_DB_CONFIG
-}
-//acJAxwz6CpBlo1hq   passowrd
-// leatherwebmaker123 username
